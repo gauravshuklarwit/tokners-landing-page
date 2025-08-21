2157d42d362a24a907de222b3cac750d8dcb993a
gauravshuklarwit <shukla.gaurav@rwit.io>
2025-08-21 18:00:20 +0530

    feat: add icons component

---
+++ src/components/icons.tsx
@@ -0,0 +1,36 @@
+type IconProps = React.HTMLAttributes<SVGElement>;
+
+export const Icons = {
+  menu: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 9h16.5m-16.5 6.75h16.5"
+      />
+    </svg>
+  ),
+  cross: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  ),
+};
