ba869aab06cc13468b46a88fd03399669141da43
gauravshuklarwit <shukla.gaurav@rwit.io>
2025-08-25 15:38:35 +0530

    feat: create date formatter component

---
+++ src/components/formatted-date.tsx
@@ -0,0 +1,20 @@
+import { formatDate } from "@/app/lib/utils";
+
+interface FormattedDateProps {
+  date: Date;
+  className?: string;
+}
+
+export const FormattedDate: React.FC<FormattedDateProps> = ({
+  date,
+  className,
+}) => {
+  const isoDate = date.toISOString().split("T")[0];
+  const displayDate = formatDate(date);
+
+  return (
+    <time dateTime={isoDate} className={className}>
+      {displayDate}
+    </time>
+  );
+};
